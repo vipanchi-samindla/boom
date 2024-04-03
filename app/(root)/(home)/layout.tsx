9e1b9fa77c78cdd34a9d427728d9420c31d5d18b
vipanchi-samindla <vipanchisamindla786@gmail.com>
2024-04-03 13:03:55 +0530

    Boom

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 
 export const metadata: Metadata = {
-  title: 'YOOM',
+  title: 'BOOM',
   description: 'A workspace for your team, powered by Stream Chat and Clerk.',
 };
 
@@ -16,7 +16,7 @@ const RootLayout = ({ children }: Readonly<{children: ReactNode}>) => {
 
       <div className="flex">
         <Sidebar />
-        
+
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">{children}</div>
         </section>
